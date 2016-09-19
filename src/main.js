be0461bce1963bf5935372ce0f3731a5e9db1481
myxingke <myxingke@126.com>
2016-09-19 17:07:53 +0800

    components

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 //noinspection JSAnnotator
 import Vue from 'vue';
 import Test from './vue/Test.vue';
-Vue.component('Test', Test);
+
 new Vue({
     el: "body",
-    data: {
-        ok: true
+    components: {
+        Test
     }
-})
+});
