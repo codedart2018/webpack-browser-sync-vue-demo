e45d0206cdbcc8a80cb4829a26e231db94736554
myxingke <myxingke@126.com>
2016-09-20 15:05:17 +0800

    store 移动到入口文件去

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 //noinspection JSAnnotator
 import Vue from 'vue';
 import App from './vue/App.vue';
+//store就是vuex里用来存储组件树用到的所有状态的对象
+import store from '../vuex/store/index';
 new Vue({
     el: "body",
     components: {
         App
-    }
+    },
+    // 在根组件加入 store，让它的子组件和 store 连接
+    store: store
 });
--- src/vue/App.vue
+++ src/vue/App.vue
@@ -13,16 +13,12 @@
     import Display from './Display.vue'
     import Increment from './Increment.vue'
     import Decrement from './Decrement.vue'
-    //store就是vuex里用来存储组件树用到的所有状态的对象
-    import store from '../vuex/store/index';
 
     export default {
         components: {
             Display: Display,
             Increment: Increment,
             Decrement //这种写法必须在ES6 babel下才行
-        },
-        // 在根组件加入 store，让它的子组件和 store 连接
-        store: store
+        }
     }
 </script>
